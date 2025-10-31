9b70c469294e971e246d3b903a3bddef8a8fd0e0
Alessia Cardinale <cardinalealessia3@gmail.com>
2025-10-31 09:40:26 +0100

    First commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[]args)
+        {
+            Dog dog = new();
+            Cat cat = new();
+            Bird bird = new();
+            Fish fish = new();
+
+            List<Animal> animals = new() { dog, cat, bird, fish };
+
+            foreach(var animal in animals)
+            {
+                Console.WriteLine($"Ecco il verso di {animal.GetType().Name}: '{animal.MakeSound()}' ed ecco cosa mangia: {animal.Eat()}");
+                if(animal is IFly flyable)
+                {
+                    flyable.Fly();
+                }
+                if(animal is ISwim swimmable)
+                {
+                    swimmable.Swim();
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp2/utils.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+        public abstract class Animal()
+        {
+            public abstract string Eat();
+            public static void Sleep()
+            {
+                Console.WriteLine("Zzz...");
+            }
+            public abstract string MakeSound();
+        }
+
+        public class Dog : Animal
+        {
+         public override string Eat()
+            {
+            return "Mangio crocchette e cibo in scatola";
+        }
+            public override string MakeSound()
+            { return "Bau Bau!";
+        }
+        }
+        public class Cat : Animal
+        {
+            public override string Eat()
+                { return "Mangio croccantini e lische di pesce";
+        }
+            public override string MakeSound()
+            {
+                return "Meow!";
+            }
+        }
+        public class Bird : Animal, IFly
+        {
+            public override string Eat()
+            {
+                return "Mangio semi e piccoli insetti";
+            }
+            public override string MakeSound()
+            {
+                return "Cip, Cip!";
+            }
+            void IFly.Fly()
+            {
+                Console.WriteLine("So volare");
+            }
+        }
+        public  class Fish : Animal, ISwim
+        {
+            public override string Eat()
+            {
+                return "Mangio gameretti e piccoli pesciolini";
+            }
+            public override string MakeSound()
+            { return "Blub, Blub!";
+        }
+            void ISwim.Swim()
+            {
+                Console.WriteLine("So nuotare");
+            }
+        }
+        public interface ISwim
+        {
+            public void Swim();
+        }
+        public interface IFly
+        {
+            public void Fly();
+        }
+    }
